814621567cc79ea935ead4df48d282f2f2d9710a
Jesus Aguirre <jaguirre2@unm.edu>
2025-03-08 20:27:03 -0700

    3/8/25 10:26

--- src/Controller.java
+++ src/Controller.java
@@ -1,2 +1,15 @@
 public class Controller {
+    private InputInterface inputInterface;
+    private Timer timer;
+    private LightController lightController;
+    private DoorMonitor doorMonitor;
+    private Cooking cooking;
+
+    public Controller(){
+        inputInterface = new InputInterface();
+        timer = new Timer();
+        lightController = new LightController();
+        doorMonitor = new DoorMonitor();
+        cooking = new Cooking();
+    }
 }
--- src/Cooking.java
+++ src/Cooking.java
@@ -1,6 +1,9 @@
 public class Cooking {
+    private TempSensor tempSensor;
+    private HeaterController heaterController;
     public Cooking(){
-
+        tempSensor = new TempSensor();
+        heaterController = new HeaterController();
     }
     private void startCooking(){
 
